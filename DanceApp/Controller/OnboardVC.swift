a5e450bd3d753e9a7ec6a862e59b0231018fe82b
raisazka <raisazka@gmail.com>
2020-06-09 14:19:30 +0700

    create base project (refactor storyboards, view controllers, and model)

--- DanceApp/Controller/OnboardVC.swift
+++ DanceApp/Controller/OnboardVC.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
---
+++ DanceApp/Model/Song.swift
@@ -0,0 +1,18 @@
+//
+//  Song.swift
+//  DanceApp
+//
+//  Created by Rais on 09/06/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Song {
+    
+    var name:String?
+    var image: UIImage?
+    var duration: Int?
+        
+}
